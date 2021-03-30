12f9765c835bea2bc776f242b015fa30e0bcdcd4
Quinten Althues <quintenra@gmail.com>
2021-03-30 20:45:01 +0200

    langpicker!

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let strings, langCode
 
-async function localise(t) {
+function localise(t) {
     let langCookie = document.cookie.split("lang=")[1]
     if (langCookie) langCookie = langCookie.split(";")[0]
+    console.log(document.cookie)
     langCode = langCookie || window.navigator.language || "en"
-    if (t > 0) langCode = window.navigator.language.split("-")[0] || "en"
-    if (t > 1) langCode = "en"
-    if (t > 2) return
+    if (t > 1) langCode = window.navigator.language.split("-")[0] || "en"
+    if (t > 2) langCode = "en"
+    if (t > 3) return
 
     fetch("https://raw.githubusercontent.com/QkeleQ10/Localisation/master/strings/" + langCode + ".json", { method: 'GET' })
         .then((response) => { return response.json() })
@@ -17,6 +18,29 @@ async function localise(t) {
             document.querySelectorAll("*[data-i18n]").forEach(e => e.innerHTML = strings[e.dataset.i18n] || e.innerHTML)
             document.querySelectorAll(".i18n").forEach(e => e.innerHTML = strings[e.innerHTML] || e.innerHTML)
         })
+}
+
+function openlangpicker() {
+    let e = document.getElementById("langpicker")
+    let langs = ["en", "nl", "de", "fr", "pt", "ru", "tr", "id", "ms"]
+    e.toggleAttribute("disabled")
+    e.innerHTML = ""
+    e.style.background = "none"
+    e.style.padding = "0"
+    langs.forEach(l => {
+        let b = document.createElement("button")
+        b.id = l
+        b.innerHTML = l.toUpperCase()
+        b.setAttribute("onclick", "picklang(this.id)")
+        e.appendChild(b)
+        b.style.margin = "0"
+        if (langCode == l) b.setAttribute("disabled", true)
+    })
+}
+
+function picklang(lang) {
+    document.cookie = `lang=${lang}`
+    window.location.reload()
 }
 
 function crowdinproj(name) {
