84840bf9683b7040a587645f508f1349ca4d2ed7
QkeleQ10 <quinten@althues.nl>
2022-02-13 20:44:32 +0100

    yes

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let strings, langCode
+const langNamesDefault = new Intl.DisplayNames([], { type: "language", fallback: "none", languageDisplay: 'standard' })
+
+if (window.matchMedia?.('(prefers-color-scheme: dark)').matches) {
+    document.getElementById("vars").href = "/css/varsDark.css"
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const stylesheet = event.matches ? "/css/varsDark.css" : "/css/varsLight.css"
+    document.getElementById("vars").href = stylesheet
+})
+
+localise(1)
 
 function localise(t) {
     let langCookie = document.cookie.split("lang=")[1]
@@ -23,40 +34,30 @@ function localise(t) {
         })
 }
 
-function openlangpicker() {
-    let langs = [["en", "English"]]
+async function openlangpicker() {
+    let langs = [{ id: "en", name: "English", ariaLabel: "English" }],
+        res1 = await fetch(`https://raw.githubusercontent.com/QkeleQ10/Localisation/master/availableLanguages.json`),
+        availableLanguages = await res1.json()
 
-    fetch(`https://raw.githubusercontent.com/QkeleQ10/Localisation/master/availableLanguages.json`)
-        .then((response) => { return response.json() })
-        .then((availableLanguages) => {
-            fetch(`https://raw.githubusercontent.com/QkeleQ10/Localisation/master/languageNames.json`)
-                .then((response) => { return response.json() })
-                .then((languageNames) => {
-                    availableLanguages.data.sort((a, b) => b.data.words.approved - a.data.words.approved).forEach(e => {
-                        if (e.data.translationProgress > 35) langs.push([e.data.languageId, languageNames[e.data.languageId] || `"${e.data.languageId}"`])
-                    })
+    availableLanguages.data.forEach(e => {
+        const id = e.data.languageId,
+            langNamesLocalised = new Intl.DisplayNames([id || "en"], { type: "language", fallback: "none", languageDisplay: 'standard' })
+        if (e.data.translationProgress > 35) langs.push({ id: id, name: langNamesLocalised.of(id) || langNamesDefault.of(id) || id, ariaLabel: langNamesDefault.of(id) })
+    })
 
-                    let p = document.getElementById("langpopup")
-                    p.classList.remove("hidden")
-                    document.querySelector(".popupbk").classList.remove("hidden")
-                    document.getElementById("langlist").innerHTML = ""
+    document.getElementById("langpopup").classList.remove("hidden")
+    document.querySelector("#popupWrapper").classList.remove("hidden")
+    document.getElementById("langlist").innerHTML = ""
 
-                    langs.forEach((l) => {
-                        let b = document.createElement("button")
-                        b.id = l[0]
-                        if (langCode === l[0]) b.setAttribute("disabled", true)
-                        b.innerHTML = l[1]
-                        b.setAttribute("onclick", "picklang(this.id)")
-                        document.getElementById("langlist").appendChild(b)
-                    })
-                })
-                .catch(() => {
-                    console.log("Loading languages: Failed.")
-                })
-        })
-        .catch(() => {
-            console.log("Loading languages: Failed.")
-        })
+    langs.forEach((l, i) => {
+        const b = document.createElement("button")
+        b.id = l.id
+        if (langCode === l.id) b.classList.add("selected")
+        b.innerHTML = l.name.capitalizeFirstLetter()
+        b.setAttribute("onclick", "picklang(this.id)")
+        b.setAttribute("aria-label", l.ariaLabel)
+        document.getElementById("langlist").appendChild(b)
+    })
 }
 
 function picklang(lang) {
@@ -70,4 +71,8 @@ function crowdinproj(name) {
 
 function newpage(page) {
     window.open(page, "_blank")
+}
+
+String.prototype.capitalizeFirstLetter = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1)
 }
